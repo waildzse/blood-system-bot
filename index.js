525dd932413ad5519f275d2fef187f951f934dbb
waildzse <67775782+waildzse@users.noreply.github.com>
2025-04-10 17:35:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+    console.log(`Blood System is online as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.TOKEN);
